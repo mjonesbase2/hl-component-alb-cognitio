eb19e08ba714b51ce3c83d096259c01642ee3713
Tarun Menon <tarunmenon95@gmail.com>
2023-02-14 16:45:22 +1100

    Removed reserved word

--- cognito.cfndsl.rb
+++ cognito.cfndsl.rb
@@ -10,7 +10,7 @@ CloudFormation do
   end
 
   Cognito_UserPoolDomain(:UserPoolDomain) do
-    Domain FnSub("${EnvironmentName}-testcognito-domain") #TODO remove this later
+    Domain FnSub("${EnvironmentName}-cf-test-domain") #TODO remove this later
     UserPoolId Ref(:UserPool) #Required
   end
 
@@ -21,6 +21,7 @@ CloudFormation do
     ProviderDetails ({
       client_id: 'my-google-client-id',
       client_secret: 'my-google-client-secret'
+      authorize_scopes: "profile email openid"
     })
   }
 
